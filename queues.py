bc029a399500a8ce1c7e672942c7d885f198ceb2
Calina Cenan <calina.cenan@gmail.com>
2015-03-21 13:41:02 +0200

    Code cleanup and so-called interface.

--- queues.py
+++ queues.py
@@ -11,9 +11,8 @@ class Banker:
     active_bankers = []
 
     def __init__(self):
-        # serialize console output, s.t. messages from threads don't intertwine
-        self.lock = threading.Lock()
         self.q = Queue()
+        self.currently_serving = None
         self.active = True
         self.number = Banker.number_of_bankers = Banker.number_of_bankers + 1
         Banker.active_bankers.append(self)
@@ -21,33 +20,22 @@ class Banker:
     def serve(self, person):
         # serve client - here we only pretend to do work by calling sleep
         time.sleep(person.time_for_service)
-        with self.lock:
-            if self.q:
-                # print who am I serving now?
-                print("Banker no. " + str(self.number) + ": ", person.name)
-            else:
-                print("Nobody loves me :(")
+        Observer.notify()
 
     def do_banky_stuff(self):
         # take the person at top of own queue and serve it
         while self.active:
             item = self.q.get()
+            self.currently_serving = item
             self.serve(item)
+            self.currently_serving = None
             self.q.task_done()
 
     def stop(self):
-        # TODO: move queue, add logic all over, only active queues to add
         print("Banker no. " + str(self.number) + ": I quit this damn job!")
         Banker.active_bankers.remove(self)
         self.active = False
-        self.spread_queue()
-
-    def spread_queue(self):
-        for elem in list(self.q.queue):
-            banker = random.choice(Banker.active_bankers)
-            banker.q.put(elem)
-
-        self.q.queue.clear()
+        Regulator.spread_queue(self)
 
 
 class Person:
@@ -63,6 +51,50 @@ class Person:
         self.name = fake_name_gen.name()
 
 
+class Observer:
+    @staticmethod
+    def notify():
+        print('-----------------------------------------------------\n')
+        for banker in Banker.active_bankers:
+            in_service = banker.currently_serving
+            print("Banker no. " + str(banker.number) + ": " +
+                  '(' + (in_service.name if in_service else "-") + ')'
+                  'has the current queue:' +
+                  ", ".join([item.name for item in list(banker.q.queue)]))
+        print('-----------------------------------------------------\n')
+
+
+class Regulator:
+    @staticmethod
+    def add_to_some_queue(elem):
+        banker = random.choice(Banker.active_bankers)
+        banker.q.put(elem)
+        Observer.notify()
+
+    @staticmethod
+    def stop_random_banker():
+        banker = random.choice(Banker.active_bankers)
+        banker.stop()
+        Observer.notify()
+
+    @staticmethod
+    def add_new_banker():
+        b = Banker()
+        t = threading.Thread(target=b.do_banky_stuff)
+        # thread dies when main thread (only non-daemon thread) exits.
+        t.daemon = True
+        t.start()
+        Observer.notify()
+
+    @staticmethod
+    def spread_queue(banker):
+        for elem in list(banker.q.queue):
+            Regulator.add_to_some_queue(elem)
+
+        banker.q.queue.clear()
+        Observer.notify()
+
+
 class Generator:
     generation_no = 0
 
@@ -73,44 +105,25 @@ class Generator:
         while Person.number < self.max_no:
             Generator.generation_no = Generator.generation_no + 1
             p = Person()
-            banker = random.choice(Banker.active_bankers)
-            banker.q.put(p)
-
-            # print the list of people that are currently in that queue
-            print("Banker no. " + str(banker.number) + ": " +
-                  'has the current queue:' +
-                  ", ".join([elem.name for elem in list(banker.q.queue)]))
+            Regulator.add_to_some_queue(p)
             time.sleep(0.1)
 
             # randomly open a new office at fourth iteration
             if Generator.generation_no == 4:
-                b = Banker()
-                t = threading.Thread(target=b.do_banky_stuff)
-                # thread dies when main thread (only non-daemon thread) exits.
-                t.daemon = True
-                t.start()
+                Regulator.add_new_banker()
 
             # randomly kill a new office at 6th generation
             if Generator.generation_no == 6:
-                banker = random.choice(Banker.active_bankers)
-                banker.stop()
+                Regulator.stop_random_banker()
 
 
 class Main:
     def run_stuff(self, no_of_threads, no_of_people):
         for i in range(no_of_threads):
-            b = Banker()
-            t = threading.Thread(target=b.do_banky_stuff)
-            # thread dies when main thread (only non-daemon thread) exits.
-            t.daemon = True
-            t.start()
-
-        start_time = time.perf_counter()
+            Regulator.add_new_banker()
 
         # I just like to use this word a lot :)
         generator = Generator(no_of_people)
         generation = threading.Thread(target=generator.generate)
         generation.daemon = True
         generation.start()
-
-        print('Total time of system:', time.perf_counter() - start_time)
